c22dbcd92c1aed544446c05c593dd60ebc2a7149
Puskar-Roy <puskarroy600@gmail.com>
2024-06-07 09:34:41 +0530

    feat : error handeler

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,13 +1,20 @@
-import express from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
+import { config } from "./config/config";
+import CheckError from "./lib/checkError";
+import errorHandler from "./middleware/errorMiddleware";
 
-const app = express();
+const app: Express = express();
 
 app.get("/", (req, res) => {
   res.status(200).json({ success: true });
 });
 
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  next(new CheckError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
-app.listen(5050,()=>{
-    console.log("Server Is Running on PORT 5050");
-    
-})
+app.use(errorHandler);
+
+app.listen(config.PORT, () => {
+  console.log(`[⚡] Server Is Running on http://localhost:${config.PORT}`);
+});
