cf9d204d47cf258cdce9fa0adb4f6572ef6d8fe0
Puskar-Roy <puskarroy600@gmail.com>
2024-06-07 09:47:21 +0530

    feat : limiter and cors

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,18 @@
 import express, { Express, Request, Response, NextFunction } from "express";
 import { config } from "./config/config";
+import corsOptions from "./lib/corsConfig";
+import limiter from "./lib/rateLimitConfig";
 import CheckError from "./lib/checkError";
 import errorHandler from "./middleware/errorMiddleware";
+import cors from "cors";
+import morgan from "morgan";
 
 const app: Express = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(config.DEV_ENV === "PROD" ? cors(corsOptions) : cors());
+app.use(limiter);
 
 app.get("/", (req, res) => {
   res.status(200).json({ success: true });
