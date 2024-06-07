169d5485a561a79368afbe07e573d06f32a4cb59
Puskar-Roy <puskarroy600@gmail.com>
2024-06-07 11:51:18 +0530

    feat : email config

--- apps/backend/src/config/config.ts
+++ apps/backend/src/config/config.ts
@@ -2,7 +2,8 @@ export const config = {
   PORT: process.env.PORT || 5050,
   DEV_ENV: process.env.DEV_ENV || "DEV",
   JWT_SECRET: "dsvsd",
-  JWT_COOKIE_EXPIRES_IN:"fdsdb",
-  EMAIL:"SV",
-  PASSWORD:"SVSV"
+  JWT_COOKIE_EXPIRES_IN: "fdsdb",
+  EMAIL: "SV",
+  PASSWORD: "SVSV",
+  BACKEND_URL:"dvsv"
 };
