8e2609f086ed557e521a7e0a988a61e13d4a1abb
Puskar-Roy <puskarroy600@gmail.com>
2024-06-10 20:19:14 +0530

    feat : post controllers

---
+++ apps/backend/src/controllers/petPostController.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from "express";
+import prisma from "../config/prisma.config";
+
+export const getAllPetPosts = async (req: Request, res: Response) => {
+  try {
+    const petPosts = await prisma.petsPost.findMany();
+    res.json(petPosts);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const createPetPost = async (req: Request, res: Response) => {
+  try {
+    const { petType, petImage, authorId, location, status, fixerId } = req.body;
+    const newPetPost = await prisma.petsPost.create({
+      data: {
+        petType,
+        petImage,
+        authorId,
+        location,
+        status,
+        fixerId,
+      },
+    });
+    res.status(201).json(newPetPost);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getPetPostById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const petPost = await prisma.petsPost.findUnique({
+      where: { id },
+    });
+    if (!petPost) {
+      res.status(404).json({ error: "PetPost not found" });
+    } else {
+      res.json(petPost);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const updatePetPost = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { petType, petImage, authorId, location, status, fixerId } = req.body;
+    const updatedPetPost = await prisma.petsPost.update({
+      where: { id },
+      data: { petType, petImage, authorId, location, status, fixerId },
+    });
+    res.json(updatedPetPost);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const deletePetPost = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    await prisma.petsPost.delete({
+      where: { id },
+    });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
